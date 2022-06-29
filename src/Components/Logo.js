571efae96f47330dc02d742d00624cde678c7367
hasibulhasananik40420 <hasibulhasananik40420@gmail.com>
2022-06-29 22:23:58 +0600

    added

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Marquee from "react-fast-marquee";
 const Logo = () => {
     return (
-        <Marquee speed={70}>
-        <div className=' hidden md:visible md:flex md:justify-center '>
-            <div className='flex items-center gap-8'>
-               <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo1.png?raw=true" alt="" />
-               <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo2.png?raw=true" alt="" />
-               <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo3.png?raw=true" alt="" />
-               <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo4.png?raw=true" alt="" />
-               <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo5.png?raw=true" alt="" />
-               <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo7.png?raw=true" alt="" />
-               <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo9.png?raw=true" alt="" />
-               
+        <Marquee speed={75}>
+            <div className=' hidden md:visible md:flex md:justify-center '>
+                <div className='flex items-center gap-8'>
+                    <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo1.png?raw=true" alt="" />
+                    <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo2.png?raw=true" alt="" />
+                    <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo3.png?raw=true" alt="" />
+                    <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo4.png?raw=true" alt="" />
+                    <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo5.png?raw=true" alt="" />
+                    <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo7.png?raw=true" alt="" />
+                    <img src="https://github.com/akashyap2013/PortFolio_Website/blob/master/img/brands/logo9.png?raw=true" alt="" />
+
+                </div>
             </div>
-        </div>
         </Marquee>
     );
 };
